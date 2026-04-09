5ba84cfbac24a06eeaad23f9da3244d32ee3ed88
Daniel Plamadeala <danielp@indsoft.ro>
2026-04-09 12:10:20 +0300

    add esbuild and separate build configs for scripts

--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -1,3 +1,5 @@
+console.log("CONTENT SCRIPT LOADED");
+
 chrome.runtime.onMessage.addListener((message) => {
   if (message.action === "fillFields") {
     fillRandomData();
@@ -5,16 +7,5 @@ chrome.runtime.onMessage.addListener((message) => {
 });
 
 function fillRandomData() {
-  const randomString = () =>
-    Math.random().toString(36).substring(2, 8);
-
-  const inputs = document.querySelectorAll<HTMLInputElement>("input");
-
-  inputs.forEach((input) => {
-    if (input.type === "text" || input.type === "email") {
-      input.value = randomString();
-    }
-  });
-
   console.log("Fields filled!");
 }
--- src/scripts/popup.ts
+++ src/scripts/popup.ts
@@ -1,13 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const button = document.getElementById("button1");
+  const button = document.getElementById("button4me");
 
   button!.addEventListener("click", async () => {
+    // get current tab
     const [tab] = await chrome.tabs.query({
       active: true,
       currentWindow: true
     });
 
     if (tab?.id) {
+      // send message to worker script
       chrome.tabs.sendMessage(tab.id, {
         action: "fillFields"
       });
